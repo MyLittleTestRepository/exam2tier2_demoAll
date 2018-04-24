cb4832e346c39fe4f223cfaf642c7f2b3f5dc3a3
Averin Dmitry <averin@intaro.email>
2018-04-24 14:32:34 +0300

    ex2-11 get site statistic

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -10,5 +10,9 @@ $APPLICATION->SetTitle("Оценка производительности 88");
 <p>/products/index.php	20.98%</p>
 <p>bitrix:catalog: 0.1151 с</p>
 </br>
-
+<p>ex2-11:</p>
+<p>/products/index.php	1.9812</p>
+<p>bitrix:news.list: 0.0509 с; Запросов: 29 (0.0175 с)</p>
+<p>bitrix:catalog.section: 0.1441 с; Запросов: 28 (0.0269 с)</p>
+</br>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
