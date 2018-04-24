fd5573e41fe6a3e105e5aba724910484d934cc16
Averin Dmitry <averin@intaro.email>
2018-04-24 14:26:28 +0300

    ex2-10 get site statistic

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -6,4 +6,9 @@ $APPLICATION->SetTitle("Оценка производительности 88");
 <p>/products/index.php	30.68%</p>
 <p>delta cache 4kb</p>
 </br>
+<p>ex2-10:</p>
+<p>/products/index.php	20.98%</p>
+<p>bitrix:catalog: 0.1151 с</p>
+</br>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
